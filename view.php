12a179bb7204fcf91c912a38c9694796d59920d7
François de Metz <fdemetz@af83.com>
2010-02-11 18:26:44 +0100

    fixed some bug with notify()

--- view.php
+++ view.php
@@ -5,7 +5,6 @@
  */
 class View
 {
-
     protected static $view_class = NULL;
 
     protected $view_object = NULL;
@@ -15,8 +14,6 @@ class View
 
     public $_js = array();
 
-    public $_notifs = array();
-
     public static function conf($conf) 
     {
         self::$conf = $conf;
@@ -25,52 +22,31 @@ class View
     public static function useLib($lib)
     {
         $class_name = ucfirst(strtolower($lib)).'View';
-        if(!class_exists($class_name)) throw new TouptiException(sprintf("The %s view adpator could not be loaded", $lib));
+        if (!class_exists($class_name)) 
+        {
+            throw new TouptiException(sprintf("The %s view adpator could not be loaded", $lib));
+        }
         self::$view_class = $class_name;
     }
 
     public function __construct($tpl = '', $params = array())
     {
         $view_class = self::$view_class;
-        
+        if (is_null($view_class))
+        { 
+            throw TouptiException("no adaptor set");
+        }
         call_user_func_array(array($view_class, 'conf'), array(self::$conf));
         $this->view_object = new $view_class($tpl, $params);
     }
 
     public function __call($name, $arguments)
     {
-        if(is_null($this->view_object)) throw TouptiException(sprintf("Could not call %s either on View nor on a Lib"));
-        return $this->view_object->$name($arguments);
-    }
-
-    /**
-     * @return Array notifs accumulator.
-     */
-    public function getNotifs()
-    {
-        return $this->_notifs;
-    }
-
-    /**
-     * @param ApiException or an array of them.
-     */
-    public function notify($notify)
-    {
-        if(is_array($notify))
-        {
-            foreach($notify as $key => $value)
-                $this->addNotify($value);
+        if (is_null($this->view_object))
+        { 
+            throw TouptiException(sprintf("Could not call %s either on View nor on a Lib", $name));
         }
-        else
-        {
-            $this->addNotify($notify);
-        }
-    }
-
-    private function addNotify($notify)
-    {
-        if (!is_null($notify))
-            $this->_notifs[]= $notify;
+        return call_user_func_array(array($this->view_object, $name), $arguments);
     }
 
     /**
--- view_libs/adaptor.php
+++ view_libs/adaptor.php
@@ -2,7 +2,8 @@
 
 abstract class ViewAdaptor
 {
-    
+    public $_notifs = array();
+
     abstract public static function conf($conf); 
 
     abstract public function __construct($tpl = '', $params = array());
@@ -12,4 +13,34 @@ abstract class ViewAdaptor
     abstract public function display($tpl = null);
 
     abstract public function fetch($tpl = null);
+
+    /**
+     * @return Array notifs accumulator.
+     */
+    public function getNotifs()
+    {
+        return $this->_notifs;
+    }
+
+    /**
+     * @param ApiException or an array of them.
+     */
+    public function notify($notify)
+    {
+        if(is_array($notify))
+        {
+            foreach($notify as $key => $value)
+                $this->addNotify($value);
+        }
+        else
+        {
+            $this->addNotify($notify);
+        }
+    }
+
+    private function addNotify($notify)
+    {
+        if (!is_null($notify))
+            $this->_notifs[]= $notify;
+    }
 }
