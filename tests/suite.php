731efe19434bb51b0396a4bd3cc698433207953a
François de Metz <fdemetz@af83.com>
2010-03-07 22:11:54 +0100

    Added tests on toupti and some cleanup.
    Contain backward incompatible change. You should set 'route_path' in configuration
     for loading route file or route file must be at ./conf/routes.php

--- tests/suite.php
+++ tests/suite.php
@@ -5,6 +5,7 @@ require_once('simpletest/autorun.php');
 require_once('../toupti.php');
 require_once('../request.class.php');
 require_once('../route.php');
+require_once('../controller.php');
 require_once('../view.php');
 require_once('../view_libs/adaptor.php');
 require_once('../view_libs/smarty.php');
@@ -18,5 +19,6 @@ class TouptiTestSuite extends TestSuite
         $this->addFile($test_dir .'/test_request.php');
         $this->addFile($test_dir .'/test_route.php');
         $this->addFile($test_dir .'/test_view.php');
+        $this->addFile($test_dir .'/test_toupti.php');
     }
 }
